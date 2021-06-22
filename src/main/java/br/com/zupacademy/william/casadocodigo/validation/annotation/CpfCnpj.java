cd53921ca62f7a6e8d0a75e1805875e27339b334
William Almeida <almeida.ws@hotmail.com>
2021-06-22 16:03:10 -0300

    refactor: cpfcnpj validator

--- src/main/java/br/com/zupacademy/william/casadocodigo/validation/annotation/CpfCnpj.java
+++ src/main/java/br/com/zupacademy/william/casadocodigo/validation/annotation/CpfCnpj.java
@@ -1,10 +1,14 @@
 package br.com.zupacademy.william.casadocodigo.validation.annotation;
 
-import br.com.zupacademy.william.casadocodigo.validation.validator.CpfCnpjValidator;
+import org.hibernate.validator.constraints.CompositionType;
+import org.hibernate.validator.constraints.ConstraintComposition;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -12,9 +16,12 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
-@Target({FIELD})
+@Target({FIELD, ElementType.PARAMETER})
 @Retention(RUNTIME)
-@Constraint(validatedBy = CpfCnpjValidator.class)
+@Constraint(validatedBy = {})
+@ConstraintComposition(CompositionType.OR)
+@CPF
+@CNPJ
 public @interface CpfCnpj {
 
     String message() default "CPF/CNPJ inválido";
